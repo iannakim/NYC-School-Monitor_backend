2535f013c32656dd73a030a64f55fa05797d75d0
iannakim <dear.hyunji@gmail.com>
2020-10-30 16:29:16 -0400

    routes for school index

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
-  resources :joiners
-  resources :options
-  resources :programs
-  resources :users
-  resources :reviews
-  resources :schools
-  resources :neighborhoods
-  resources :boroughs
+  # resources :joiners
+  # resources :options
+  # resources :programs
+  # resources :users
+  # resources :reviews
+  # resources :schools
+  # resources :neighborhoods
+  # resources :boroughs
+
+  get "/schools", to: "schools#index"
+  get "/keep_logged_in", to: "users#keep_logged_in"
+  post "/login", to: "users#login"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
